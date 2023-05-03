64e6bacbc332191853c33f38e61e3237a4924806
John Crow <johncrow86@hotmail.com>
2023-05-02 20:05:54 -0500

    v1.2

--- resources/script.js
+++ resources/script.js
@@ -1,21 +1,13 @@
-// function to generate random message
-const generateMessage = () => {
-    let randomNumber = Math.floor(Math.random() * 5);
-    console.log(randomNumber);
-    switch (randomNumber) {
-        case 0: return "Today will be an exceptional day";
-        break;
-        case 1: return "Today will be the worst day of the week";
-        break;
-        case 2: return "Today will be a rather normal day";
-        break;
-        case 3: return "Today will be pretty good";
-        break;
-        case 4: return "Today will be not great";
-        break;
-        default: return "ERROR";
-        break;
-    }
+let obj1 = {
+    name: 'hello',
+    number: '8',
+    location: 'NW'
 }
 
-console.log(generateMessage());
+let name = 'hey'
+let number ='5'
+let location = 'E'
+
+obj1 = {number, location, name}
+
+console.log(obj1)
